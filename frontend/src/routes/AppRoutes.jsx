0aa7f7865e6c5971b758b62e824ba6dd98b89735
Shivansh Rathor <shivanshrathore61@gmail.com>
2025-09-17 23:29:52 +0530

    Register_Login

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Login from '../screens/Login'
+import Register from '../screens/Register'
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
         <Routes>
             <Route path='/' element={<div>home</div>}></Route>
+            <Route path='/login' element={<Login></Login>}></Route>
+            <Route path='/register' element={<Register></Register>}></Route>
         </Routes>
     </BrowserRouter>
   )
